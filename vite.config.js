56922d0fc3afedd6389b330a59e57997731a3a18
992manav <dmanavpianist@gmail.com>
2024-07-26 19:49:45 +0530

    some more changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/vite-react-router/"
+  base:"/vite-react-router/",
+  build: {
+    outDir: 'dist', // Ensure the build output is in the 'dist' directory
+  },
 })
+
 
